1b41ddb5bc74691bf3fbe7129c8da12ad31dd771
Shimmy <shimmyrukundo@gmail.com>
2024-05-20 16:15:25 -0400

    configure ui github page

--- docs/README.md
+++ docs/README.md
@@ -1,6 +1,4 @@
-# Shimmy Rukundo
-
-Shimmy Rukundo
+# create-svelte
 
 Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/main/packages/create-svelte).
 
--- docs/svelte.config.js
+++ docs/svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
-const dev = process.argv.includes('dev');
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,14 +8,10 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		appDir: 'app', // Required as the default is _app
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		paths: {
-            base: dev ? '/ui' : process.env.BASE_PATH,
-        }
+		adapter: adapter()
 	}
 };
 
